b32b647e18378fa3b1e23d927fe6f58b757fba36
lordsenna <sebastian.strid@gmail.com>
2021-03-31 09:47:19 +0100

    Ev ändr

--- GUI1.py
+++ GUI1.py
@@ -220,7 +220,7 @@ def fyllMaskinInfo(self):
      cursor.execute('SELECT * FROM maskinregister WHERE Maskinnummer = ' + maskinnummer + ';')
      maskinInfo = cursor.fetchone()
      maskinInfo = list(maskinInfo)
-     
+
      try:
           entMaskinnummermaskininfo.config(state=NORMAL)
           entMaskinnummermaskininfo.delete(0, 'end')
@@ -2473,7 +2473,7 @@ def sparaHistorik(maskinnummer):
 db = mysql.connector.connect(
      host = "localhost",
      user = "root",
-     password = "Not1but2",
+     password = "sennaa66",
      database = "tschakt"
 )
 cursor = db.cursor()
@@ -2578,7 +2578,7 @@ ScbLbDelagaresMaskiner.config(command =LbMaskiner.yview)
 LbDelagaresMaskiner.config(yscrollcommand=ScbLbDelagaresMaskiner.set)
 
 #Maskinbild
-img = Image.open("1.jpg")  
+img = Image.open("c:/filer/OTAD/OTAD/1.jpg")  
 img = img.resize((225,200), Image. ANTIALIAS)
 img2 = ImageTk.PhotoImage(img)
 img_label = Label(frameOvrigText, image=img2)
